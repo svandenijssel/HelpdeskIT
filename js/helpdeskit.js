90a27c6e5c05e54b4bc8f3f390584102eb63d832
Sander van den Ijssel <sander@vinton.nl>
2018-07-05 16:24:04 +0200

    WIP

--- js/helpdeskit.js
+++ js/helpdeskit.js
@@ -2,6 +2,35 @@ $(document).ready(function(){
    setTimeout(function(){
        $(".display-3").removeClass('invisible').addClass('animated fadeInUp');
     }, 500);
+    
+    $( ".modal-call" ).click(function() {
+        var meldingid = $(this).attr('data-id');
+        var setlog='';
+        
+        if(meldingid in meldinglog){
+            setlog += '<ul class="list-group">';
+            $.each( meldinglog[meldingid], function( key, data ) {
+                setlog += '<li class="list-group-item d-flex justify-content-between align-items-center">'+data["omschrijving"]+'<span class="badge badge-light badge-pill"><i class="icon ion-ios-calendar pr-3"></i>'+data['datum']+'</span></li>';
+            });
+            setlog += '</ul>';
+            $('.modal-trigger .modal-content .modal-body').html(setlog);
+        };
+        
+        $('.modal-trigger').modal('show');
+    });
+
+    
 });
+var options = {
+  valueNames: [ 'meta', 'titel', 'operator-naam' ]
+};
+var userList = new List('meldingen', options);
 
-
+$( ".search" ).keyup(function() {
+    console.log('ping');
+  if( $(this).val() ) {
+    $(".jumbotron").css('display','none');
+  }else{
+    $(".jumbotron").css('display','block');
+  }
+});
